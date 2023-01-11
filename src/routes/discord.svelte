07e24d91bfb4fcd81e1667e753c053c274c2d64b
funnyboy-roks <funnyboyroks@gmail.com>
2023-01-11 14:52:10 -0600

    Add discord redirect

---
+++ src/routes/discord.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <meta http-equiv="refresh" content="0; url='https://api.funnyboyroks.com/discord'" />
+</svelte:head>
+
+<div>
+
+    <p>Redirecting to <a href="https://api.funnyboyroks.com/discord">Discord</a>...</p>
+    
+</div>
